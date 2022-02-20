7bf0c9b4a437e357b587381600ccf6d0fa9efbda
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2022-02-20 17:22:51 +0000

    실패 v0.1 나도모르게 스택으로 개발해버림

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 N, M = list(map(int, input().split()))
 field = []
-notVisited=[[]*N for i in range(M-1)]
+notVisited=[]
 visited = [(0,0)]
 result = 0
 for i in range(N):
 	for j in range(M):
 		if i != N:
-			notVisited[i].append((i,j))
+			notVisited.append((i,j))
 	tlist = list(map(int, input()))
 	field.append(tlist)
 	print()
@@ -18,11 +18,11 @@ input()
 def bfs(visited):
 	while len(visited) > 0:
 		position = visited.pop()
-		
 		global result
 		result+=1
 	
-		if position == (N,M) :
+		if position == (N-1,M-1) :
+			break
 			return result
 		
 		x, y = position[0], position[1]
@@ -32,13 +32,21 @@ def bfs(visited):
 		right = (x, y+1)
 		
 		if x+1 < N and field[x+1][y]==1 and down not in visited:
+			print("go down")
 			visited.append(down)
+			field[x+1][y]=0
 		if x-1 > 0 and field[x-1][y]==1 and up not in visited:
+			print("go up")
 			visited.append(up)
+			field[x-1][y]=0
 		if y+1 < M and field[x][y+1]==1 and right not in visited:
+			print("go right")
 			visited.append(right)
+			field[x][y+1]=0
 		if y-1 > 0 and field[x][y-1]==1 and left not in visited:
+			print("go left")
 			visited.append(left)
+			field[x][y-1]=0
 		print(visited)
 bfs(visited)
 print(result)
