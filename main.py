c969dc0cd08017dc88c1d952cd65f166820bae1b
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2022-02-20 17:54:50 +0000

    성공. 정답을 참조하여 내 코드 방식대로 수정함 ㅠ

--- main.py
+++ main.py
@@ -3,24 +3,22 @@ from collections import deque
 N, M = list(map(int, input().split()))
 field = []
 visited = deque([(0,0)])
-result = 0
+result = 1
 for i in range(N):
 	tlist = list(map(int, input()))
 	field.append(tlist)
 	print()
 
 print("field",field)
-input()
 
 def bfs(visited):
 	while len(visited) or visited.pop > 0:
-		position = visited.popleft()	
-		global result
+		position = visited.popleft()
 		if position == (N-1,M-1) :
-			break
-			return result
+			return field[position[0]][position[1]]
 		
 		x, y = position[0], position[1]
+		
 		down = (x+1, y)
 		up = (x-1, y)
 		left = (x, y-1)
@@ -29,22 +27,20 @@ def bfs(visited):
 		if x+1 < N and field[x+1][y]==1 and down not in visited:
 			print("go down")
 			visited.append(down)
-			field[x+1][y]=0
+			field[x+1][y]=field[x][y]+1
 		if x-1 >= 0 and field[x-1][y]==1 and up not in visited:
 			print("go up")
 			visited.append(up)
-			field[x-1][y]=0
+			field[x-1][y]=field[x][y]+1
 		if y+1 < M and field[x][y+1]==1 and right not in visited:
 			print("go right")
 			visited.append(right)
-			field[x][y+1]=0
+			field[x][y+1]=field[x][y]+1
 		if y-1 >= 0 and field[x][y-1]==1 and left not in visited:
 			print("go left")
 			visited.append(left)
-			field[x][y-1]=0
-		print(visited)
-		
-		result+=1
+			field[x][y-1]=field[x][y]+1
+		print(visited, "step=", field[x][y])
+	return field[x][y]
 
-bfs(visited)
-print(result)
+print(bfs(visited))
